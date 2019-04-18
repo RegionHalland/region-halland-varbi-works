f0ab261d9f04ed5d66b5636be951cd3619db013e
rhn014 <Roland.Hyden@RegionHalland.se>
2019-04-18 11:25:54 +0200

    data-filer

---
+++ incs/Varbi.php
@@ -0,0 +1,178 @@
+	<?php
+/**
+ * Class för att hantera Varbi-data
+ * 
+ * @package  Region Halland
+ * @category Varbi
+ * @author   Roland Hyden <Roland.Hyden@RegionHalland.se>
+ * @link     Roland.hyden@RegionHalland.se
+ */
+class Varbi
+{
+    public function __construct() {
+    }
+	
+    public function prepareVarbiData() {
+		
+        // Variabler för url till Varbi
+        $myFeed = env('VARBI_PROD_FEED');
+        $myToken = env('VARBI_TOKEN');
+        $myPath = $myFeed . "?token=" . $myToken; 
+        
+        // Idag
+        $today = date("Y-m-d");
+		
+        // Url till Varbi komplett array
+        $strVarbiFilePath = ENV("VARBI_DATA_PATH") . "varbi_komplett.txt";
+        
+        // Kolla om fil finns
+        $varbiFleExist = file_exists($strVarbiFilePath);
+        
+
+        if (!$varbiFleExist){
+        	
+            // Hämta data från Varbi		
+            $arrData = file_get_contents($myPath);
+
+            // Decoda data till rätt json-format
+            $arrDecodedData = json_decode($arrData, true);
+
+            // Skapa fil
+            $myVarbiFile = fopen($strVarbiFilePath, "w");
+
+            // Skapa en ny array med token & expire
+            $mySaveVarbi = array();
+            $mySaveVarbi['data'] = $arrDecodedData;
+            $mySaveVarbi['today'] = $today;
+
+            $mySerializedSaveVarbi = serialize($mySaveVarbi);
+            
+            // Spara biljett i fil
+            fwrite($myVarbiFile, $mySerializedSaveVarbi);
+
+            $myPositionLista = array();
+            $myPosition = array();
+            
+            foreach ($arrDecodedData['positions'] as $data) {
+                
+                // Data för varje enskilt jobb
+                $myPosition['id'] = $data['id'];    
+                $myPosition['name'] = $data['name'];    
+                $myPosition['title'] = $data['title'];  
+                $myPosition['town'] = $data['town'];    
+                $myPosition['country'] = $data['country'];  
+                $myPosition['published'] = $data['published'];  
+                $myPosition['lastday'] = $data['ends']; 
+                $myPosition['ref_no'] = $data['ref_nr'];    
+                $myPosition['hours'] = $data['hours'];  
+                $myPosition['type'] = $data['type'];    
+                $myPosition['description'] = $data['description'];  
+                $myPosition['working_hours'] = $data['working_hours'];
+                $myPosition['applyURI'] = $data['applyURI'];
+                
+                $myPositionEnskildPath = ENV("VARBI_DATA_PATH") . "varbi_". $data['id'] .".txt";
+                $myPositionEnskildFile = fopen($myPositionEnskildPath, "w");
+                $mySerializedPositionEnskild = serialize($myPosition);
+                fwrite($myPositionEnskildFile, $mySerializedPositionEnskild);
+                
+                array_push($myPositionLista, array(
+                    'id' => $data['id'],
+                    'name' => $data['name'],
+                    'title' => $data['title'],
+                    'town' => $data['town'],
+                    'country' => $data['country'],
+                    'published' => $data['published'],
+                    'lastday' => $data['ends'],
+                    'ref_no' => $data['ref_nr'],
+                    'hours' => $data['hours'],
+                    'type' => $data['type'],
+                    'working_hours' => $data['working_hours'],
+                    'applyURI' => $data['applyURI']
+                ));
+
+            }
+            
+            $myPositionListaPath = ENV("VARBI_DATA_PATH") . "varbi_lista.txt";
+            $myPositionListaFile = fopen($myPositionListaPath, "w");
+            $mySerializedPositionLista = serialize($myPositionLista);
+            fwrite($myPositionListaFile, $mySerializedPositionLista);
+		
+        } else {
+        	
+            // Öppna filen
+            $myVarbiFile = fopen($strVarbiFilePath, "r");
+
+            // Läs innehåll
+            $myVarbiFileContent = fread($myVarbiFile,filesize($strVarbiFilePath));
+
+            // Unserializera filens innehåll
+            $mySerializedVarbiFileContent = unserialize($myVarbiFileContent);
+
+            if ($today != $mySerializedVarbiFileContent['today']) {
+
+                // Hämta data från Varbi        
+                $arrData = file_get_contents($myPath);
+
+                // Decoda data till rätt json-format
+                $arrDecodedData = json_decode($arrData, true);
+
+                // Skapa fil
+                $myVarbiFile = fopen($strVarbiFilePath, "w");
+
+                // Skapa en ny array med token & expire
+                $mySaveVarbi = array();
+                $mySaveVarbi['data'] = $arrDecodedData;
+                $mySaveVarbi['today'] = $today;
+
+                $mySerializedSaveVarbi = serialize($mySaveVarbi);
+            
+                // Spara biljett i fil
+                fwrite($myVarbiFile, $mySerializedSaveVarbi);
+
+                $myPositionLista = array();
+                $myPosition = array();
+            
+                foreach ($arrDecodedData['positions'] as $data) {
+                
+                    // Data för varje enskilt jobb
+                    $myPosition['id'] = $data['id'];    
+                    $myPosition['name'] = $data['name'];    
+                    $myPosition['title'] = $data['title'];  
+                    $myPosition['town'] = $data['town'];    
+                    $myPosition['country'] = $data['country'];  
+                    $myPosition['published'] = $data['published'];  
+                    $myPosition['lastday'] = $data['ends']; 
+                    $myPosition['ref_no'] = $data['ref_nr'];    
+                    $myPosition['hours'] = $data['hours'];  
+                    $myPosition['type'] = $data['type'];    
+                    $myPosition['description'] = $data['description'];  
+                    $myPosition['working_hours'] = $data['working_hours'];
+                    $myPosition['applyURI'] = $data['applyURI'];
+                
+                    $myPositionEnskildPath = ENV("VARBI_DATA_PATH") . "varbi_". $data['id'] .".txt";
+                    $myPositionEnskildFile = fopen($myPositionEnskildPath, "w");
+                    $mySerializedPositionEnskild = serialize($myPosition);
+                    fwrite($myPositionEnskildFile, $mySerializedPositionEnskild);
+                
+                    array_push($myPositionLista, array(
+                        'id' => $data['id'],
+                        'name' => $data['name'],
+                        'title' => $data['title']
+                    ));
+
+                }
+            
+                $myPositionListaPath = ENV("VARBI_DATA_PATH") . "varbi_lista.txt";
+                $myPositionListaFile = fopen($myPositionListaPath, "w");
+                $mySerializedPositionLista = serialize($myPositionLista);
+                fwrite($myPositionListaFile, $mySerializedPositionLista);
+
+            }
+
+        }
+
+        return "OK!";
+	
+	}
+
+}
--- region_halland_varbi_works.php
+++ region_halland_varbi_works.php
@@ -12,28 +12,121 @@
 	Text Domain: regionhalland
 	*/
 
-	// Returnera lediga jobb (endast test-data)
-	function get_region_halland_varbi_works_test() {
-		
-		// Hämta data från Varbi		
-		$arrData = file_get_contents(env('VARBI_TEST_FEED'));
-		
-		// Decoda data till rätt json-format
-		$arrDecodedData = json_decode($arrData, true);
-		
-		// Returnera data
-		return $arrDecodedData;
-	}
+		// vid 'init', anropa funktionen region_halland_register_utbildning()
+	add_action('init', 'region_halland_register_lediga_jobb');
 
+	// Denna funktion registrerar en ny post_type och gör den synlig i wp-admin
+	function region_halland_register_lediga_jobb() {
+		
+		// Vilka labels denna post_type ska ha
+		$labels = array(
+	        'name' => _x( 'Lediga jobb', 'Post type general name', 'textdomain' ),
+	        'singular_name' => _x( 'Lediga jobb', 'Post type singular name', 'textdomain' )
+	    );
+		
+		// Inställningar för denna post_type 
+	    $args = array(
+	        'labels' => $labels,
+	        'rewrite' => array('slug' => 'alla-lediga-jobb'),
+			'show_ui' => false,
+			'has_archive' => true,
+			'publicly_queryable' => true,
+			'public' => true,
+			'show_in_menu' => false,
+			'query_var' => false,
+			'menu_icon' => 'dashicons-megaphone',
+	        'supports' => array( 'title', 'editor', 'author', 'thumbnail')
+	    );
+
+	    // Registrera post_type
+	    register_post_type('alla-lediga-jobb', $args);
+	    
+	}
+		
+	// Returnera lediga jobb (endast test-data)
+	function get_region_halland_varbi_works($type = 1, $id = 0) {
+		
+		// Variabel för sökväg
+    	if (!defined('VARBI_DIR')) {
+	    	define('VARBI_DIR', __DIR__);
+		}
+
+    	// Autoload all classes in incs-folder
+    	spl_autoload_register(function($className) {
+			include_once VARBI_DIR . '/incs/' . $className . '.php';
+		});
+
+		// Preparera data
+		$objVarbi = new Varbi();
+		$intVarbi = $objVarbi->prepareVarbiData();
+
+		if ($type == 1) {
+			$strVarbiFilePath = ENV("VARBI_DATA_PATH") . "varbi_lista.txt";
+		} else {
+			$strVarbiFilePath = ENV("VARBI_DATA_PATH") . "varbi_".$id.".txt";
+		}
+		
+		$myVarbifile = fopen($strVarbiFilePath, "r");
+		$myVarbiFileContent = fread($myVarbifile,filesize($strVarbiFilePath));
+		$myUnSerializedVarbiFileContent = unserialize($myVarbiFileContent);
+		
+		// Returnera aktuell enhet
+		return $myUnSerializedVarbiFileContent;
+
+	}
 
 	// Metod som anropas när pluginen aktiveras
 	function region_halland_varbi_works_activate() {
-		// Ingenting just nu...
+		
+		// Definiera 'post_title'
+		$myPageTitle = "Visa ledigt jobb";
+		
+		// Kontrollera om denna post redan finns
+		$ifExist = post_exists($myPageTitle);
+
+		// Om posten inte finns så skapa en ny post
+		if ($ifExist == 0) {
+			
+			// Definiera en array med aktuella värden
+			$postarr = array();
+			$postarr['post_title'] = "Visa ledigt jobb";
+			$postarr['post_name'] = "ett-ledigt-jobb";
+			$postarr['post_status'] = "publish";
+			$postarr['post_type'] = "alla-lediga-jobb";
+			$postarr['post_parent'] = 999999999;
+
+			// Skapa en ny post
+			wp_insert_post($postarr, $wp_error = false);
+		
+		}
+
+		// Vi aktivering, registrera post_type "utbildning"
+		region_halland_register_lediga_jobb();
+
+		// Tala om för wordpress att denna post_type finns
+		// Detta gör att permalink fungerar
+	    flush_rewrite_rules();
+	
 	}
 
 	// Metod som anropas när pluginen avaktiveras
 	function region_halland_varbi_works_deactivate() {
-		// Ingenting just nu...
+		
+		// Definiera 'post_title'
+		$myPageTitle = "Visa ledigt jobb";
+		
+		// Definiera 'post_type'
+		$myPostType = "alla-lediga-jobb";
+
+		// Hämta post
+		$myPost = get_page_by_title($myPageTitle, "", $myPostType);
+		
+		// Plocka ut ID ur objektet
+		$myID = $myPost->ID;
+
+		// Radera aktuell post
+		wp_delete_post($myID, $force_delete = true);
+	
 	}
 	
 	// Vilken metod som ska anropas när pluginen aktiveras
